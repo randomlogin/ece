b1047606a6f2e32627fabb84957811848f23e6e9
Alexander Shevtsov <randomlogin76@gmail.com>
2015-12-24 04:28:12 +0300

    added tests, resolved bug with nonce generation with \x00 byte

--- lib/ece/ece.rb
+++ lib/ece/ece.rb
@@ -15,7 +15,7 @@ class ECE
     OpenSSL::HMAC.digest(digest, key, input)
   end
 
-  def self.hkdf_extract(salt, ikm)
+  def self.hkdf_extract(salt, ikm) #ikm stays for input keying material
     hmac_hash(salt,ikm)
   end
 
@@ -31,23 +31,16 @@ class ECE
     output = nonce.dup
     integer = nonce[-6..-1].unpack('B*')[0].to_i(2) #taking last 6 bytes, treating as integer
     x = ((integer ^ counter) & 0xffffff) + ((((integer / 0x1000000) ^ (counter / 0x1000000)) & 0xffffff) * 0x1000000)
-    output[-6..-1] = [x.to_s(16)].pack('H*')
-    output
-  end
-
-  def self.encrypt_record(params, counter, buffer, pad=0)
-    raise "Key must be #{KEY_LENGTH} bytes long" unless params[:key].length == KEY_LENGTH
-    gcm = OpenSSL::Cipher.new('aes-128-gcm')
-    gcm.encrypt
-    gcm.key = params[:key]
-    gcm.iv = generate_nonce(params[:nonce], counter)
-    enc = gcm.update("\x00"+buffer) + gcm.final + gcm.auth_tag  #enc = gcm.update("\x00"*pad+buffer)+gcm.final + gcm.auth_tag padding is not fully implemented for now
-    enc
+    bytestring = x.to_s(16).length < 12 ? "0"*(12-x.to_s(16).length)+x.to_s(16) : x.to_s(16) #it's for correct handling of cases when generated integer is less than 6 bytes
+    output[-6..-1] = [bytestring].pack('H*')                                                 #without it packing would produce less than 6 bytes
+    output                                                                                   #I didn't find pack directive for such usage, so there is a such solution
   end
 
   def self.encrypt(data, params)
     key = extract_key(params)
-    rs = 4095 #should be variable, but for now it's constant
+    rs = params[:rs] ? params [:rs] : 4096
+    raise "The rs parameter must be greater than 1." if rs <= 1
+    rs -=1 #this ensures encrypted data cannot be truncated
     result = ""
     counter = 0
     (0..data.length).step(rs) do |i|
@@ -58,23 +51,12 @@ class ECE
     result
   end
 
-  def self.decrypt_record(params, counter, buffer, pad=0)
-    raise "Key must be #{KEY_LENGTH} bytes long" unless params[:key].length == KEY_LENGTH
-    gcm = OpenSSL::Cipher.new('aes-128-gcm')
-    gcm.decrypt
-    gcm.key = params[:key]
-    gcm.iv = generate_nonce(params[:nonce], counter)
-    gcm.auth_tag = buffer[-TAG_LENGTH..-1]
-    decrypted = gcm.update(buffer[0..-TAG_LENGTH-1]) + gcm.final
-    #padding = decrypted[0]  -- this would be used once variable record-size is implemented
-    #padding_length = decrypted[0].unpack("C")
-    #raise Err unless padding = "\x00"*padding_length
-    decrypted[1..-1]
-  end
-
   def self.decrypt(data, params)
     key = extract_key(params)
-    rs = 4096+16 #not changeable for now
+    rs = params[:rs] ? params [:rs] : 4096
+    raise "The rs parameter must be greater than 1." if rs <= 1
+    rs += 16
+    raise "Message is truncated" if data.length % rs == 0
     result = ""
     counter = 0
     (0..data.length).step(rs) do |i|
@@ -84,5 +66,31 @@ class ECE
     end
     result
   end
+
+  def self.decrypt_record(params, counter, buffer, pad=0)
+    gcm = OpenSSL::Cipher.new('aes-128-gcm')
+    gcm.decrypt
+    gcm.key = params[:key]
+    gcm.iv = generate_nonce(params[:nonce], counter)
+    raise "Block is too small" if buffer.length <= TAG_LENGTH+1
+    gcm.auth_tag = buffer[-TAG_LENGTH..-1]
+    decrypted = gcm.update(buffer[0..-TAG_LENGTH-1]) + gcm.final
+    padding_length = decrypted[0].unpack("C")[0]
+    raise "Padding is too big" if padding_length+1 > decrypted.length
+    padding = decrypted[1..padding_length]
+    raise "Wrong padding"  unless padding = "\x00"*padding_length
+    decrypted[1..-1]
+  end
+
+  def self.encrypt_record(params, counter, buffer, pad=0)
+    gcm = OpenSSL::Cipher.new('aes-128-gcm')
+    gcm.encrypt
+    gcm.key = params[:key]
+    gcm.iv = generate_nonce(params[:nonce], counter)
+    gcm.auth_data = "" 
+    enc = gcm.update("\x00"+buffer) + gcm.final + gcm.auth_tag  #enc = gcm.update("\x00"*pad+buffer)+gcm.final + gcm.auth_tag padding is not fully implemented for now
+    enc
+  end
+
 
 end
---
+++ test/ece_test.rb
@@ -0,0 +1,51 @@
+require 'test/unit'
+require 'ece'
+
+class TestECE < Test::Unit::TestCase
+
+  def encrypt_decrypt(len=rand(1..5000), params={key: Random.new.bytes(16), salt:Random.new.bytes(16)})
+    input = Random.new.bytes(len)
+    encrypted = ECE.encrypt(input, params)
+    decrypted = ECE.decrypt(encrypted, params)
+    assert_equal(input, decrypted)
+  end
+
+  def test_simple_workflow
+    (0..20).each do |i|
+      encrypt_decrypt
+    end
+  end
+
+  def test_wrong_record_size_
+    assert_raise(RuntimeError) {encrypt_decrypt(rand(1..5000), {salt: Random.new.bytes(16), key: Random.new.bytes(16), rs: 1}) }
+  end
+
+
+  def test_wrong_salt
+    assert_raise(RuntimeError) {encrypt_decrypt(rand(1..5000), {salt: Random.new.bytes(10), key: Random.new.bytes(20)}) }
+  end
+
+  def test_trancated_message
+    rs = rand(2..8000)
+    len = rand(10*(rs+16)..20*(rs+16))
+    truncation = rand(1..10)*(rs+16)-1
+    params = {salt: Random.new.bytes(16), key: Random.new.bytes(16), rs: rs}
+    assert_raise(RuntimeError) do
+      encrypted = ECE.encrypt(Random.new.bytes(len), params)
+      ECE.decrypt(encrypted[0..truncation], params)
+    end
+  end
+
+  def test_message_is_broken
+    rs = rand(2..8000)
+    len = rand(10*(rs+16)..20*(rs+16))
+    truncation = rand(1..10)*(rs+16)+rand(16)
+    params = {salt: Random.new.bytes(16), key: Random.new.bytes(16), rs: rs}
+    assert_raise(RuntimeError) do
+      encrypted = ECE.encrypt(Random.new.bytes(len), params)
+      ECE.decrypt(encrypted[0..truncation], params)
+    end
+  end
+
+
+end
